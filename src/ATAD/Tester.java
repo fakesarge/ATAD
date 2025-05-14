7c8920ccb2e246a80a827a478ef032c91e4171b1
fakesarge <sargentxtrad@gmail.com>
2025-05-14 08:20:19 -0400

    Update Tester.java

--- src/ATAD/Tester.java
+++ src/ATAD/Tester.java
@@ -6,7 +6,7 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+			sfsdf
 		PasswordGeneratorMethod one = new PasswordGeneratorMethod(8,true,true,true,true);
 		System.out.println(one.getPassword());
 
